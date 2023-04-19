284bd7aa84cefa0e7e8bcd9767f760ec0f628395
pooja <poojakarekallu@gmail.com>
2023-04-19 13:13:00 +0530

    change

---
+++ area.py
@@ -0,0 +1,11 @@
+b=int(input("enter base: "))
+h=int(input("enter heigth: "))
+areaoftriangle=1/2*b*h
+print(areaoftriangle)
+l=int(input("enter length: "))
+w=int(input("enter width: "))
+areaofrect=l*w
+print(areaofrect)
+s=int(input("enter side: "))
+areaoftri=s*s
+print(areaoftri)
